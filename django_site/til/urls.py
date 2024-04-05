e39327b7adc8e49abf71f3f1ac07be0795bbfa35
Akhaled19 <akhaled@clarku.edu>
2024-04-04 23:52:26 -0400

    fixes endpoint name tag

--- django_site/til/urls.py
+++ django_site/til/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = "til"  # this registers the namespace for reverse lookups
 urlpatterns = [
-    path("edit/<int:learnt_id>", views.learning_data_entry, name="til_edit_id"),
-    path("add", views.learning_data_entry, name="til_add"),
+    path("edit/<int:learnt_id>", views.learning_data_entry, name="til_de_id"),
+    path("add", views.learning_data_entry, name="til_de"),
     path("show/<int:learnt_id>", views.show, name="til_show"),
     path("tagged/", views.landing_page, name="til_show"),
     path("tagged/<str:tagstring>", views.landing_page, name="til_show_tagged"),
